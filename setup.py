e7e3a7485faddef9b53a2435c809dcbe2dbd67f5
Felipe Ortiz <fortizc@gmail.com>
2018-01-03 21:07:13 -0300

    Update version

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ with open(path.join(here, 'requirements.txt'), encoding='utf-8') as f:
 setup(
     name='Micro-dev',
 
-    version='0.0.1',
+    version='1.0.0',
 
     description='Library to create microservices for Micro',
     long_description=long_description,
