d8e3d0d7facfa93e9c9cb402641ab03a1c7b40f9
Pablo Ahumada <pablo.ahumadadiaz@gmail.com>
2018-06-16 16:02:16 -0400

    Update Micro Celery endpoints
    Now, the Celery endpoints are methods of Requests class, so you need to
    instance it givin the 'broker_url' and the 'queue' name in order to do
    it

--- micro/api/endpoints.py
+++ micro/api/endpoints.py
@@ -1,33 +1,27 @@
-import os
 from celery import Celery
 
-BROKER_URL = os.environ.get("MICRO_BROKER_URL")
-QUEUE = os.environ.get("MICRO_QUEUE")
-
-if not BROKER_URL:
-    raise RuntimeError("No broker URL set")
-
-if not QUEUE:
-    raise RuntimeError("No queue set")
-
-app = Celery("Micro", broker=BROKER_URL, backend="rpc://")
+app = Celery("Micro", backend="rpc://")
 
 
-@app.task(name="Micro.plugins", queue=QUEUE)
-def plugins():
-    pass
+class Requests:
+    def __init__(self, broker, queue):
+        app.conf.update(
+            broker_url=broker,
+            task_routes={"Micro.*": {"queue": queue}}
+        )
 
+    @app.task(name="Micro.plugins")
+    def plugins():
+        pass
 
-@app.task(name="Micro.info", queue=QUEUE)
-def info(name):
-    pass
+    @app.task(name="Micro.info")
+    def info(name):
+        pass
 
+    @app.task(name="Micro.help")
+    def help(name):
+        pass
 
-@app.task(name="Micro.help", queue=QUEUE)
-def help(name):
-    pass
-
-
-@app.task(name="Micro.run", queue=QUEUE)
-def run(plugin_name, **kwargs):
-    pass
+    @app.task(name="Micro.run")
+    def run(plugin_name, **kwargs):
+        pass
